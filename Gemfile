ca5928e066931bb486a5d6b3e5947b8cb775ee15
danmalmx <danmalmx@gmail.com>
2018-10-17 20:58:00 +0200

    installing API gem, adding API modul and  API feed

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'turbolinks', '~> 5'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'news-api'
 
 group :development, :test do
   gem 'coveralls', require: false
--- app/services/article_service.rb
+++ app/services/article_service.rb
@@ -1,1 +1,20 @@
+require 'news-api'
+require 'json'
+require 'date'
 
+now = Date.today
+month = now-30
+
+module FeedsService  
+  
+  def self.get_feed(query, source, domain)
+    newsapi = News.new(Rails.application.credentials.news[:api_key])
+    newsapi.get_everything(q: query,
+                            sources: source,
+                            domains: domain,
+                            from: Date.today,
+                            to: month,
+                            language: 'en',
+                            sortBy: 'relevancy'))
+  end
+end
