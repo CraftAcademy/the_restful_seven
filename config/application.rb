7c48e7b51f3fdd602570961cdae42058cba3204a
rinbo <robin.b@outlook.com>
2018-10-17 17:04:15 +0200

    Fix broken code in application.rb - missing local iterator variable

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module TheRestfulSeven
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.generators do |generate|
       generate.helper false
       generate.assets false
       generate.view_specs false
@@ -23,5 +23,6 @@ module TheRestfulSeven
       generate.routing_specs false
       generate.controller_specs false
       generate.system_tests false
+    end
   end
 end
