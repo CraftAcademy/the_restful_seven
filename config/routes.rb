85cd940d753238f664dfbbdc7d109556dafc8dfe
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-18 16:44:19 +0200

    Remove commented out code from routes and get api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'api_feed/index'
   root controller: :api_feed, action: :index
-  # resources :api_feeds, action: :index
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
