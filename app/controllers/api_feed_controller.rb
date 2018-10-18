25a6d232f794ce18db45bf0298fe4bb10a181d8f
Zak <zakary.howard94@gmail.com>
2018-10-18 16:00:53 +0200

    refactored method to add pagesize

--- app/controllers/api_feed_controller.rb
+++ app/controllers/api_feed_controller.rb
@@ -1,8 +1,8 @@
 class ApiFeedController < ApplicationController
 
   def index
-    @apifeed = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk')
-    @top_article = FeedService.top_news('tech', 'bbc-news', 'bbc.co.uk')
+    @apifeed = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk', 10)
+    @top_article = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk', 1)
   end
 
 end
--- app/services/feed_service.rb
+++ app/services/feed_service.rb
@@ -2,32 +2,15 @@ require 'news-api'
 require 'json'
 require 'date'
 
-now = Date.today
-month = now-30
-
 module FeedService
   
-  def self.get_feed(query, source, domain)
+  def self.get_feed(query, source, domain, pagesize)
     newsapi = News.new(Rails.application.credentials.news[:api_key])
     newsapi.get_everything(q: query,
                             sources: source,
                             domains: domain,
+                            pageSize: pagesize,
                             language: 'en')
-                            # sortBy: 'relevancy')
                             
   end
-
-  def self.top_news(query, source, domain)
-    newsapi = News.new(Rails.application.credentials.news[:api_key])
-    newsapi.get_everything(q: query,
-                           sources: source,
-                           domains: domain,
-                           language: 'en',
-                           pageSize: 1)
-
-  end
 end
-
-
-                            # from: Date.today,
-                            # to: month,
