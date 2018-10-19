f0332794d64aa6a0a228641de82c0c1e3ecf1318
Zak <zakary.howard94@gmail.com>
2018-10-19 10:01:09 +0200

    refactored api call for top article to not use two requests but rather call on the first article of the api feed

--- app/controllers/api_feed_controller.rb
+++ app/controllers/api_feed_controller.rb
@@ -2,7 +2,7 @@ class ApiFeedController < ApplicationController
 
   def index
     @apifeed = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk', 10)
-    @top_article = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk', 1)
+    @top_article = @apifeed.first
   end
 
 end
