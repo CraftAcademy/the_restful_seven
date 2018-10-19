db98e67c9f400dff12e1ae34fcb59cb6b81a5a94
danmalmx <danmalmx@gmail.com>
2018-10-19 12:56:47 +0200

    showing top news as one article only

--- app/controllers/api_feed_controller.rb
+++ app/controllers/api_feed_controller.rb
@@ -1,8 +1,7 @@
 class ApiFeedController < ApplicationController
 
   def index
-    @apifeed = FeedService.get_feed('tech', 'bbc-news', 'bbc.co.uk', 10)
+    @apifeed = FeedService.get_feed('politics', 'bbc-news', 'bbc.co.uk', 10)
     @top_article = @apifeed.first
   end
-
 end
