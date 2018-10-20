3348a2a5b828398a2c49d276faf5a6261595d01d
rinbo <robin.b@outlook.com>
2018-10-20 14:17:53 +0200

    Add rspec test for article model and styling for new creation form

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'should contain the following columns' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :author }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
 end
