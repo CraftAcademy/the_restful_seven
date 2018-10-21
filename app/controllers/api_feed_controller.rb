a387dd086d1373d2c7da252aa5e7b580a2c99364
Zak <zakary.howard94@gmail.com>
2018-10-21 11:33:29 +0200

    removed api feed controller reason being no need to have an extra

--- app/controllers/api_feed_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApiFeedController < ApplicationController
-
-  def index
-    @apifeed = FeedService.get_feed('politics', 'bbc-news', 'bbc.co.uk', 10)
-    @top_article = @apifeed.first
-  end
-end
