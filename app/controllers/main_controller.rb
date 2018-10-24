5f438b15b1e9a891e7564d1064aea074b952e523
Magnus <magnus.thor.ar@gmail.com>
2018-10-24 11:26:04 +0200

    WIP setup dropdown of news categories

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,13 +2,14 @@ class MainController < ApplicationController
 
   def index
     @allfeed = FeedService.get_feed('all', 'bbc-news', 'bbc.co.uk', 10) 
-    binding.pry   
-    @generalfeed = FeedService.get_feed('general', 'bbc-news', 'bbc.co.uk', 10)    
-    @businessfeed = FeedService.get_feed('business', 'bbc-news', 'bbc.co.uk', 10)    
-    @sportsfeed = FeedService.get_feed('sports', 'bbc-news', 'bbc.co.uk', 10)    
-    @entertainmentfeed = FeedService.get_feed('entertainment', 'bbc-news', 'bbc.co.uk', 10)    
-    @healthfeed = FeedService.get_feed('health', 'bbc-news', 'bbc.co.uk', 10)
-    @technologyfeed = FeedService.get_feed('technology', 'bbc-news', 'bbc.co.uk', 10)
+    
+    if params[:category]
+      @category_selected = params[:category]
+      @category_news = FeedService.get_feed(params[:category], 'bbc-news', 'bbc.co.uk', 10) 
+    else
+      @category_selected = "all"
+      @category_news = @allfeed
+    end
 
     @top_article = @allfeed.first
   end
