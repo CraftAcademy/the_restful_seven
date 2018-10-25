f151ff22dac9fc5b4e483dfcd4e5a5fc47344ed5
rinbo <robin.b@outlook.com>
2018-10-25 13:48:08 +0200

    Generate migration for user model that adds role column with integer datatype add enum for roles in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  enum role: { user: 0, premium_user: 1, author: 2, editor: 3 }
+  
 end
