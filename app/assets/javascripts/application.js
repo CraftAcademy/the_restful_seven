8a7a4d479bfd525920c888b271f8ade3f4498e70
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-28 15:25:28 +0100

    Added stripe form to subscription page and added javascript and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,61 @@
 //= require turbolinks
 //= require_tree .
 
+const initiateStripe = () => {
+
+var stripe = Stripe('pk_test_YNLQm57HHhtQoCSUtcAFxR02');
+
+var elements = stripe.elements();
+
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+var card = elements.create('card', {style: style});
+
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+}
+
 document.addEventListener('turbolinks:load', ()=>{
     App.windowContent = document.getElementById('content')
+    let stripeForm = document.getElementById('payment-form')
+    if (stripeForm) {
+        initiateStripe();
+    }
 })
