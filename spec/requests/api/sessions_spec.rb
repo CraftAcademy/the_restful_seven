f034373874d49dfd67f481de9aac6ae0329ee34d
rinbo <robin.b@outlook.com>
2018-10-29 22:03:11 +0100

    Removed factory bot from sessions create method

--- spec/requests/api/sessions_spec.rb
+++ spec/requests/api/sessions_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
 
   describe 'POST /api/auth/sign_in' do
